0539871ccbb31283bc674f9d1928f9b6b8c7022d
Mike Caines <michael.caines@cossette.com>
2018-10-09 09:44:37 -0300

    basic open & close state, animation

--- src/Solarfield/Controls/DialogControl.js
+++ src/Solarfield/Controls/DialogControl.js
@@ -2,9 +2,10 @@ define(
 	[
 		'solarfield/ok-kit-js/src/Solarfield/Ok/ObjectUtils',
 		'solarfield/controls/src/Solarfield/Controls/Control',
+		'solarfield/ok-kit-js/src/Solarfield/Ok/CssLoader',
 		'solarfield/ok-kit-js/src/Solarfield/Ok/AnimUtils',
 	],
-	function (ObjectUtils, Control, AnimUtils) {
+	function (ObjectUtils, Control, CssLoader, AnimUtils) {
 		"use strict";
 	
 		var DialogControl = ObjectUtils.extend(Control, {
@@ -15,26 +16,26 @@ define(
 			animateOpen: function () {
 				var popup = this.getElement();
 
-				var promise = AnimUtils.onAnimationEnd(popup, 'dialogControlPopupOpen')
-				.then(function () {
-					AnimUtils.setAnimation(popup, null);
-					popup.dataset.controlAnimation = '';
-					
-					//workaround for IE11 layout not updating
-					if (self.navigator.userAgent.indexOf('Trident/7.0') > -1) {
-						var x = document.body.style.fontSize;
-						document.body.style.fontSize = '1px';
-						document.body.style.fontSize = x;
-					}
-				});
-
-				popup.dataset.controlAnimation = 'opening';
-
-				AnimUtils.setAnimation(popup, {
-					name: 'dialogControlPopupOpen',
-					duration: getComputedStyle(popup).animationDuration,
-					fillMode: 'forwards'
-				});
+				var promise = AnimUtils.onAnimationEnd(popup, 'dialogControlOpen')
+					.then(function () {
+						AnimUtils.setAnimation(popup, null);
+						popup.dataset.controlAnimation = '';
+						
+						//workaround for IE11 layout not updating
+						if (self.navigator.userAgent.indexOf('Trident/7.0') > -1) {
+							var x = document.body.style.fontSize;
+							document.body.style.fontSize = '1px';
+							document.body.style.fontSize = x;
+						}
+					});
+	
+					popup.dataset.controlAnimation = 'opening';
+	
+					AnimUtils.setAnimation(popup, {
+						name: 'dialogControlOpen',
+						duration: getComputedStyle(popup).animationDuration,
+						fillMode: 'forwards'
+					});
 
 				return promise;
 			},
@@ -44,25 +45,25 @@ define(
 			 * @protected
 			 */
 			animateClose: function () {
-				var popup = this.getElement().querySelector('.dialogControlPopup');
+				var popup = this.getElement();
 
-				var promise = AnimUtils.onAnimationEnd(popup, 'dialogControlPopupClose')
-				.then(function () {
-					AnimUtils.setAnimation(popup, null);
-					popup.dataset.controlAnimation = '';
-					
-					//workaround for IE11 layout not updating
-					if (self.navigator.userAgent.indexOf('Trident/7.0') > -1) {
-						var x = document.body.style.fontSize;
-						document.body.style.fontSize = '1px';
-						document.body.style.fontSize = x;
-					}
-				});
+				var promise = AnimUtils.onAnimationEnd(popup, 'dialogControlClose')
+					.then(function () {
+						AnimUtils.setAnimation(popup, null);
+						popup.dataset.controlAnimation = '';
+						
+						//workaround for IE11 layout not updating
+						if (self.navigator.userAgent.indexOf('Trident/7.0') > -1) {
+							var x = document.body.style.fontSize;
+							document.body.style.fontSize = '1px';
+							document.body.style.fontSize = x;
+						}
+					});
 
 				popup.dataset.controlAnimation = 'closing';
 
 				AnimUtils.setAnimation(popup, {
-					name: 'dialogControlPopupClose',
+					name: 'dialogControlClose',
 					duration: getComputedStyle(popup).animationDuration,
 					fillMode: 'forwards'
 				});
@@ -71,17 +72,37 @@ define(
 			},
 			
 			open: function (aOptions) {
+				this.getElement().setAttribute('aria-hidden', 'false');
 				this.animateOpen();
 			},
 			
 			close: function (aOptions) {
+				this.getElement().setAttribute('aria-hidden', 'true');
 				this.animateClose();
+			},
+			
+			toggle: function (aOptions) {
+				var hidden = this.getElement().getAttribute('aria-hidden') === 'true';
+				
+				if (hidden) {
+					this.getElement().setAttribute('aria-hidden', 'false');
+					this.animateOpen();
+				}	else {
+					this.getElement().setAttribute('aria-hidden', 'true');
+					this.animateClose();
+				}
+			},
+			
+			hookup: function (aOptions) {
+				return DialogControl.super.prototype.hookup.apply(this, arguments)
+				.then(function () {
+					return CssLoader.import('solarfield/controls/src/Solarfield/Controls/style/dialog-control');
+				}.bind(this))
 			},
 			
 			constructor: function (aOptions) {
 				DialogControl.super.call(this, aOptions);
-				
-				//TODO
+				this.close();
 			}
 		});
 		
