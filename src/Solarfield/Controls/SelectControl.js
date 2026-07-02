2bf942dee20598079938559bb328a04fd5b6ba6d
Mike Caines <michael.caines@cossette.com>
2026-07-02 13:08:02 -0300

    Revert "adjust input-source/pointer/focus handling to fix firefox issue"
    This reverts commit 3cc836637781d55489f8339c391f68d463efc754.

--- src/Solarfield/Controls/SelectControl.js
+++ src/Solarfield/Controls/SelectControl.js
@@ -75,6 +75,7 @@ define(
 
 			_scsc_handleSelectKeypress: function () {
 				this.setInputSource('keyboard');
+				this.setPointerType(null);
 
 				this._scsc_syncFocusables();
 
@@ -91,13 +92,6 @@ define(
 				}
 
 				this.setInputSource('pointer');
-				this._scsc_syncFocusables();
-			},
-			
-			_scsc_handleWidgetMouseover: function () {
-				this.setInputSource('pointer');
-				this.setPointerType('mouse');
-				this._scsc_syncFocusables();
 			},
 
 			_scsc_handleWidgetKeypress: function (aEvt) {
@@ -114,6 +108,7 @@ define(
 				}
 
 				this.setInputSource('keyboard');
+				this.setPointerType(null);
 			},
 
 			_scsc_handleItemLabelClick: function (aEvt) {
@@ -595,7 +590,6 @@ define(
 
 					container.classList.add('selectControl');
 					container.dataset.controlOpen = 0;
-					container.addEventListener('mouseover', this._scsc_handleWidgetMouseover);
 
 					var selectEl = container.querySelector('select');
 					var multiple = selectEl.multiple;
@@ -708,7 +702,6 @@ define(
 				this._scsc_handleSelectClick = this._scsc_handleSelectClick.bind(this);
 				this._scsc_handleSelectKeypress = this._scsc_handleSelectKeypress.bind(this);
 				this._scsc_handleWidgetClick = this._scsc_handleWidgetClick.bind(this);
-				this._scsc_handleWidgetMouseover = this._scsc_handleWidgetMouseover.bind(this);
 				this._scsc_handleWidgetKeypress = this._scsc_handleWidgetKeypress.bind(this);
 				this._scsc_handleItemLabelClick = this._scsc_handleItemLabelClick.bind(this);
 				this._scsc_handleItemButtonClick = this._scsc_handleItemButtonClick.bind(this);
